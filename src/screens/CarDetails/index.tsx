efd23463bb1548f67b11d1b524676f4848fc23df
willian-onuki <willianonuki1@gmail.com>
2022-11-21 22:44:46 -0300

    feat: implemented scroll view animation in Car Details screen

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,30 +1,22 @@
 import React from 'react';
-import { ListRenderItem, StatusBar } from 'react-native';
+import { StatusBar, StyleSheet } from 'react-native';
 
-import {
-  Container,
-  Header,
-  CarImage,
-  Content,
-  DataSheetGrid,
-  Details,
-  Car,
-  Brand,
-  Name,
-  Rent,
-  Period,
-  Price,
-  About,
-  Footer,
-} from './styles';
-import { DataSheet, DataSheetProps } from '../../components/DataSheet';
-import { BackButton } from '../../components/BackButton';
-import { ImageSlider } from '../../components/ImageSlider';
-import { Button } from '../../components/Button';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import Animated, { Extrapolate, interpolate, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../components/BackButton';
+import { Button } from '../../components/Button';
+import { DataSheet } from '../../components/DataSheet';
+import { ImageSlider } from '../../components/ImageSlider';
 import { CarDTO } from '../../dtos/CarDTO';
 import { CarDetailsProps } from '../../global/routes';
 import { getAccessories } from '../../utils/getAccessories';
+import {
+  About, Brand, Car, CarImage, Container, DataSheetGrid,
+  Details, Footer, Header, Name, Period,
+  Price, Rent
+} from './styles';
 
 interface RouteParam {
   car: CarDTO;
@@ -33,24 +25,67 @@ interface RouteParam {
 export function CarDetails() {
   const navigation = useNavigation();
   const route = useRoute<CarDetailsProps>();
-
+  const theme = useTheme();
   const { car } = route.params as RouteParam;
+
+  const scrollVertically = useSharedValue(0);
+  const scrollHandler = useAnimatedScrollHandler(event => {
+    scrollVertically.value = event.contentOffset.y;
+  });
+
+  const headerStyleAnimated = useAnimatedStyle(() => {
+    return {
+      height: interpolate(scrollVertically.value,
+        [0, 200],
+        [200, 70],
+        Extrapolate.CLAMP
+      )
+    };
+  });
+
+  const sliderCarsStyleAnimated = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(scrollVertically.value,
+        [0, 150],
+        [1, 0],
+        Extrapolate.CLAMP
+      )
+    };
+  });
 
   const handleConfirmRental = () => {
     navigation.navigate('Scheduling', {
       car
     });
-  }
+  };
 
   return (
     <Container>
-      <Header>
-        <BackButton onPress={() => navigation.goBack()} />
-      </Header>
-      <CarImage>
-        <ImageSlider imageUrl={car.photos} />
-      </CarImage>
-      <Content>
+      <StatusBar
+        barStyle='dark-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      <Animated.View style={[headerStyleAnimated, styled.header, {backgroundColor: theme.colors.background_primary}]}>
+        <Header>
+          <BackButton onPress={() => navigation.goBack()} />
+        </Header>
+        <Animated.View style={sliderCarsStyleAnimated}>
+          <CarImage>
+            <ImageSlider imageUrl={car.photos} />
+          </CarImage>
+        </Animated.View>
+      </Animated.View>
+      <Animated.ScrollView contentContainerStyle={{
+        paddingHorizontal: 24,
+        paddingTop: getStatusBarHeight() + 160,
+        alignItems: 'center',
+      }}
+        showsVerticalScrollIndicator={false}
+        onScroll={scrollHandler}
+        //in scrollEventThrottle use 16, 'cause 1000 ms divides by 60 will be 16, so with this value we will have 60 frames per second
+        scrollEventThrottle={16}
+      >
         <Details>
           <Car>
             <Brand>
@@ -77,8 +112,13 @@ export function CarDetails() {
 
         <About>
           {car.about}
+          {car.about}
+          {car.about}
+          {car.about}
+          {car.about}
+          {car.about}
         </About>
-      </Content>
+      </Animated.ScrollView>
 
       <Footer>
         <Button title='Escolher período do aluguel' onPress={handleConfirmRental} />
@@ -86,3 +126,11 @@ export function CarDetails() {
     </Container>
   )
 }
+
+const styled = StyleSheet.create({
+  header: {
+    position: 'absolute',
+    overflow: 'hidden',
+    zIndex: 1,
+  }
+});
--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -32,14 +32,6 @@ export const DataSheetGrid = styled.View`
   margin-left: 8px;
 `;
 
-export const Content = styled.ScrollView.attrs({
-  contentContainerStyle: {
-    padding: 24,
-    alignItems: 'center',
-  },
-  showsVerticalScrollIndicator: false,
-})``
-
 export const Details = styled.View`
   width: 100%;
   flex-direction: row;
